bf5f268de1deea37c7005ecfbefc71ceb910a23a
Robert Wild <wildr2@rpi.edu>
2015-10-31 12:36:12 -0400

    Start In Game menu

--- RGS2015Unity/Assets/Scripts/Menu/Pages/StartPopupPage.cs
+++ RGS2015Unity/Assets/Scripts/Menu/Pages/StartPopupPage.cs
@@ -3,11 +3,14 @@ using System.Collections;
 
 public class StartPopupPage : UIMenuPage
 {
-    public UIMenuPage fade_page;
+    //public UIMenuPage fade_page;
+    public UIMenuPage loading_page;
 
 	public void StartMatch()
     {
-        fade_page.on_transitioned_in = new System.Action(() => Application.LoadLevel("Game"));
-        fade_page.TransitionIn();
+        this.TransitionOut();
+        loading_page.on_transitioned_in = new System.Action(() => Application.LoadLevel("Game"));
+        loading_page.TransitionIn();
+        //fade_page.TransitionIn();
     }
 }
