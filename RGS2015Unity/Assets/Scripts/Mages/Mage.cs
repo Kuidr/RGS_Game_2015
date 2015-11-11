ad39ef0cd540bb187e93e11d90c1b385f2f58275
Robert Wild <wildr2@rpi.edu>
2015-11-11 12:47:27 -0500

    Make mage 'explode' on death
    mage death applies force to nearby resources

--- RGS2015Unity/Assets/Scripts/Mages/Mage.cs
+++ RGS2015Unity/Assets/Scripts/Mages/Mage.cs
@@ -241,6 +241,40 @@ public class Mage : MonoBehaviour
     }
     private void Die()
     {
+        Explode(10, 10);
+    }
+    public void Explode(float radius, float max_force)
+    {
+
+        Collider2D[] cols = Physics2D.OverlapCircleAll(transform.position, radius);
+        foreach (Collider2D col in cols)
+        {
+            // Projectiles / ball
+            if (col.CompareTag("Projectile") || col.CompareTag("Ball"))
+            {
+                // Apply force in radius (exponentially decreasing with distance)
+                Rigidbody2D r = col.GetComponent<Rigidbody2D>();
+                Vector2 v = r.position - (Vector2)transform.position;
+                float force = (1 - Mathf.Pow(v.magnitude / radius, 2f)) * max_force;
+                r.AddForceAtPosition(v.normalized * force, transform.position, ForceMode2D.Impulse);
+                continue;
+            }
+
+            // Resources
+            else if (col.CompareTag("Crystal") || col.CompareTag("Rock"))
+            {
+                Resource res = col.GetComponent<Resource>();
+                Vector2 v = col.transform.position - transform.position;
+                float break_radius = radius / 1.5f;
+
+                if (v.magnitude < break_radius)
+                {
+                    res.Break();
+                    float force = (1 - Mathf.Pow(v.magnitude / break_radius, 2f)) * max_force;
+                    col.GetComponent<Rigidbody2D>().AddForceAtPosition(v.normalized * force, transform.position, ForceMode2D.Impulse);
+                }
+            }
+        }
     }
 
     private void OnCastSpell()
