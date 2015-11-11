16b1f2220c58984b6812c80e85a14b9faf74cff0
Robert Wild <wildr2@rpi.edu>
2015-11-11 12:41:54 -0500

    Add crystalization spell

---
+++ RGS2015Unity/Assets/Scripts/Magic/Spell Effects/SEProduceCrystals.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SEProduceCrystals : SpellEffect
+{
+    public int num;
+
+    public override void Do(Mage caster, Spell origin_spell)
+    {
+        caster.AddCrystals(num);
+    }
+}
