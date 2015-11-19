d7dd5f6f60cc66c12e719f56812da945fe0c06fa
Robert Wild <wildr2@rpi.edu>
2015-11-19 12:07:09 -0500

    Make start button pause in game

--- RGS2015Unity/Assets/Scripts/Menu/PauseController.cs
+++ RGS2015Unity/Assets/Scripts/Menu/PauseController.cs
@@ -7,10 +7,12 @@ public class PauseController : MonoBehaviour
     public MatchManager match_manager;
     public bool Paused { get; private set; }
 
-
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape) && match_manager.GetMatchState() == MatchState.InMatch)
+        if ((Input.GetKeyDown(KeyCode.Escape) ||
+            Input.GetButtonDown("Start1") ||
+            Input.GetButtonDown("Start2"))
+            && match_manager.GetMatchState() == MatchState.InMatch)
         {
             if (Paused && pause_menu.IsTopPage() && !pause_menu.Hidden()) pause_menu.ButtonResume();
             else
